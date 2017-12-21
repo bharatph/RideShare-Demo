2628ea010bb52343d8f65d3702abf83c63e8cfb2
Bharatvaj <bharatvajph@gmail.com>
2017-12-21 21:30:04 +0530

    Patching from <Goutham SM> local commit - Map Implementation

--- app/src/main/java/com/cz/rideshare/MainActivity.java
+++ app/src/main/java/com/cz/rideshare/MainActivity.java
@@ -1,9 +1,18 @@
 package com.cz.rideshare;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -27,12 +36,29 @@ public class MainActivity extends AppCompatActivity
     private NavigationView navigationView = null;
     private ImageButton fab = null;
 
-    private GoogleMap mMap = null;
+
+    private GoogleMap mMap;
+    LocationListener locationListener;
+    LocationManager locationManager;
 
     void initialize(){
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         fab = (ImageButton) findViewById(R.id.fab);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults.length > 0) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                }
+            }
+        }
     }
 
     @Override
@@ -85,15 +111,62 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Your location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,17.0f));
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+//        if(Build.VERSION.SDK_INT<=23)
+//        {
+//            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+//            }
+//        }
+//        else {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastLocation != null) {
+                LatLng sydney = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17.0f));
+            }
+        }
+//        }
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(13.356946, 80.142909);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
     }
 }
